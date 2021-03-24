77c78eee8571b8ed1368c14449b62c37b97ff979
Mohitp98 <mohit.patil@gslab.com>
2021-03-24 19:02:54 +0530

    Added Component for search-box

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import './App.css';
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -16,10 +18,18 @@ class App extends Component {
   }
 
   render() {
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+      );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}>
-        </CardList>        
+        <SearchBox 
+          placeholder='search monsters'
+          handleChange={e => this.setState({searchField: e.target.value})}
+        />        
+        <CardList monsters={filteredMonsters}/>       
       </div>
     );  
   }

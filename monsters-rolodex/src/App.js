ac83e2913854335206e8aaa279195f47bb5e0720
Mohitp98 <mohit.patil@gslab.com>
2021-03-24 20:04:01 +0530

    Added css for app.js

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -4,11 +4,16 @@ import { CardList } from './components/card-list/card-list.component'
 import { SearchBox } from './components/search-box/search-box.component'
 class App extends Component {
   constructor() {
+    // constructor is running very first
+
     super();
+    // defined state for this component
     this.state = {
       monsters: [],
       searchField: ''
     };
+
+    // this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -16,18 +21,23 @@ class App extends Component {
     .then(response => response.json()
     .then(users => this.setState( {monsters: users} )))
   }
+  // class method
+  handleChange = (e) => {   
+    this.setState({searchField: e.target.value});
+  };
 
   render() {
     const {monsters, searchField} = this.state;
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
       );
-
+    
     return (
       <div className="App">
+        <h1>Monster Rolodex</h1>
         <SearchBox 
           placeholder='search monsters'
-          handleChange={e => this.setState({searchField: e.target.value})}
+          handleChange={this.handleChange}
         />        
         <CardList monsters={filteredMonsters}/>       
       </div>
